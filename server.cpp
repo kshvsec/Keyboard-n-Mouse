4643809ca192f5e0f676b6796594b4932c3e7406
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-02-26 01:39:54 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,5 +1,11 @@
 #include "controls/controllers.h"
 #include <iostream>
 
+// cd "e:\codes\remote\" ; if ($?) { g++ server.cpp controls/controllers.cpp -o server } ; if ($?) { .\server }
+
+using namespace std;
+
 int main(){
+    std::this_thread::sleep_for(5000ms);
+    leftClick();
 }
