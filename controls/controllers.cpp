aa0108a4a5ebe8f42f6064749e78cc162d7acb15
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-02-26 01:41:00 +0530

    Update controllers.cpp

--- controls/controllers.cpp
+++ controls/controllers.cpp
@@ -8,9 +8,12 @@ void moveMouse(int x, int y){
     SetCursorPos(x, y);
 }
 
-void lefiClick(){
+void leftClick(){
     INPUT input;
     input.type = 0;
-    input.mi.
-
+    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(1, &input, sizeof(INPUT));
 }
