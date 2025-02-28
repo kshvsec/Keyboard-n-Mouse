6944e0c07d708766fe89bf13ef671585160e2319
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-02-28 20:09:00 +0530

    Update controllers.cpp

--- controls/controllers.cpp
+++ controls/controllers.cpp
@@ -8,12 +8,44 @@ void moveMouse(int x, int y){
     SetCursorPos(x, y);
 }
 
-void leftClick(){
+void leftClick() {
     INPUT input;
     input.type = 0;
-    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    input.mi.dwFlags = 0x0002; //MOUSEEVENTF_LEFTDOWN
+    SendInput(1, &input, sizeof(INPUT));
+    sleepInMs(100);
+    input.mi.dwFlags = 0x0004; //MOUSEEVENTF_LEFTUP
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void holdLeftClick(int time){
+    INPUT input;
+    input.type = 0;
+    input.mi.dwFlags = 0x0002; //MOUSEEVENTF_LEFTDOWN
+    SendInput(1, &input, sizeof(INPUT));
+    sleepInS(time);
+    input.type = 0;
+    input.mi.dwFlags = 0x0004; //MOUSEEVENTF_LEFTUP
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void rightClick() {
+    INPUT input;
+    input.type = 0;
+    input.mi.dwFlags = 0x0008; //MOUSEEVENTF_RIGHTDOWN;
     SendInput(1, &input, sizeof(INPUT));
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    input.mi.dwFlags = 0x0010; // MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void holdRightClick() {
+    INPUT input;
+    input.type = 0;
+    input.mi.dwFlags = 0x0008; //MOUSEEVENTF_RIGHTDOWN
+    SendInput(1, &input, sizeof(INPUT));
+    sleepInS(time);
+    input.type = 0;
+    input.mi.dwFlags = 0x0010; //MOUSEEVENTF_RIGHTUP
     SendInput(1, &input, sizeof(INPUT));
 }
