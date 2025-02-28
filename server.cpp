c070bb2628f8a820ad453dd585bce826de75f254
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-03-01 01:01:49 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,5 +1,10 @@
 #include "controls/controllers.h"
 #include <iostream>
 
+// g++ server.cpp controls/controllers.cpp -o server
+
 int main(){
+    // testing
+    std::this_thread::sleep_for(1000ms);
+    SpecialKeys("BACKSPACE");
 }
