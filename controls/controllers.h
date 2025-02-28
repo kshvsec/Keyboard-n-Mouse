1be2d274fe7c83cce3c401e9bbb9ae2bf67b5cf2
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-02-28 23:28:38 +0530

    Update controllers.h

--- controls/controllers.h
+++ controls/controllers.h
@@ -5,8 +5,11 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <unordered_map>
 
 // oneline defined functions for thread sleep management
+
+typedef std::unordered_map<std::string, std::string> dict; // for adding and removing specialkeys
 
 #define sleepInS(x) std::this_thread::sleep_for(std::chrono::seconds(x));
 #define sleepInMs(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
@@ -19,7 +22,7 @@ void holdLeftClick(int time); // in seconds
 void rightClick();
 void holdRightClick(int time); // in seconds
 
-void pressKey(std::string key);
-void typeString(const std::string text);
+void SpecialKeys(std::string key);
+void typeString(std::string text);
 
 #endif
