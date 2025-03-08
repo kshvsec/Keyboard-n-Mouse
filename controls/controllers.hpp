3c096eded62b58c741016d6a128f8abddf36f4b3
K5HV <115079501+kshvsec@users.noreply.github.com>
2025-03-09 00:53:28 +0530

    Update controllers.hpp

--- controls/controllers.hpp
+++ controls/controllers.hpp
@@ -9,6 +9,8 @@
 
 // oneline defined functions for thread sleep management
 
+typedef std::unordered_map<std::string, WORD> dict; // for adding and removing specialkeys
+
 #define sleepInS(x) std::this_thread::sleep_for(std::chrono::seconds(x));
 #define sleepInMs(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
 
